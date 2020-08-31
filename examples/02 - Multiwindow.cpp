e7bebeb31f0cc89651a78c60c2157a411c80bc50
Oier Lauzirika Zarrabeitia <oierlauzi@riseup.net>
2020-08-31 13:20:20 +0200

    Added a multi window example

---
+++ examples/02 - Multiwindow.cpp
@@ -0,0 +1,105 @@
+/*
+ * This example shows how multiple windows can be instantiated
+ * 
+ * How to compile:
+ * c++ 02\ -\ Multiwindow.cpp -std=c++17 -Wall -Wextra -lzuazo -lzuazo-window -lglfw -ldl -lpthread
+ */
+
+#include "TestSource.h"
+
+#include <zuazo/Instance.h>
+#include <zuazo/Outputs/Window.h>
+#include <zuazo/Signal/Delay.h>
+
+#include <mutex>
+
+int main() {
+	//Load all the required window components. This *MUST* be done before instantiating Zuazo
+	Zuazo::Outputs::Window::init();
+
+	//Instantiate Zuazo as usual
+	Zuazo::Instance::ApplicationInfo appInfo {
+		"Window Example 02",						//Application's name
+		Zuazo::Version(0, 1, 0),					//Application's version
+		Zuazo::Verbosity::GEQ_INFO,					//Verbosity 
+		Zuazo::VideoMode::ANY,						//Default video-mode
+		Zuazo::Instance::defaultInstanceLogFunc,	//Instance log callback
+		Zuazo::Instance::defaultElementLogFunc		//Element log callback
+	};
+	Zuazo::Instance instance(std::move(appInfo));
+	std::unique_lock<Zuazo::Instance> lock(instance);
+
+	//Generaly, you want to enable event polling
+	Zuazo::Outputs::Window::enableRegularEventPolling(instance);
+
+	//Construct the desired video mode
+	const Zuazo::VideoMode videoMode(
+		Zuazo::Utils::MustBe<Zuazo::Rate>(Zuazo::Rate(25, 1)), //Just specify the desired rate
+		Zuazo::Utils::Any<Zuazo::Resolution>(),
+		Zuazo::Utils::Any<Zuazo::AspectRatio>(),
+		Zuazo::Utils::Any<Zuazo::ColorPrimaries>(),
+		Zuazo::Utils::Any<Zuazo::ColorModel>(),
+		Zuazo::Utils::Any<Zuazo::ColorTransferFunction>(),
+		Zuazo::Utils::Any<Zuazo::ColorSubsampling>(),
+		Zuazo::Utils::Any<Zuazo::ColorRange>(),
+		Zuazo::Utils::Any<Zuazo::ColorFormat>()	
+	);
+
+	//Construct the window objects
+	Zuazo::Outputs::Window window1(
+		instance, 						//Instance
+		"Output Window 1",				//Layout name
+		Zuazo::Math::Vec2i(1280, 720),	//Window size (in screen coordinates)
+		Zuazo::Outputs::Window::NO_MONITOR, //No monitor
+		videoMode						//Video mode limits
+	);
+
+	Zuazo::Outputs::Window window2(
+		instance, 						//Instance
+		"Output Window 2",				//Layout name
+		Zuazo::Math::Vec2i(1280, 720),	//Window size (in screen coordinates)
+		Zuazo::Outputs::Window::NO_MONITOR, //No monitor
+		videoMode						//Video mode limits
+	);
+
+
+	//Open the windows (now becomes visible)
+	window1.open();
+	window2.open();
+
+	//Let's route a test source to the first window
+	const Zuazo::VideoMode testVideoMode(
+		Zuazo::Utils::MustBe<Zuazo::Rate>(Zuazo::Rate(25, 1)), //Just specify the desired rate
+		Zuazo::Utils::MustBe<Zuazo::Resolution>(Zuazo::Resolution(1280, 720)),
+		Zuazo::Utils::MustBe<Zuazo::AspectRatio>(Zuazo::AspectRatio(1, 1)),
+		Zuazo::Utils::MustBe<Zuazo::ColorPrimaries>(Zuazo::ColorPrimaries::BT709),
+		Zuazo::Utils::MustBe<Zuazo::ColorModel>(Zuazo::ColorModel::RGB),
+		Zuazo::Utils::MustBe<Zuazo::ColorTransferFunction>(Zuazo::ColorTransferFunction::IEC61966_2_1),
+		Zuazo::Utils::MustBe<Zuazo::ColorSubsampling>(Zuazo::ColorSubsampling::RB_444),
+		Zuazo::Utils::MustBe<Zuazo::ColorRange>(Zuazo::ColorRange::FULL),
+		Zuazo::Utils::MustBe<Zuazo::ColorFormat>(Zuazo::ColorFormat::B8G8R8A8)	
+	);
+
+	TestSource testSrc(instance, "", testVideoMode);
+	testSrc.open();
+	Zuazo::Signal::getInput<Zuazo::Video>(window1) << Zuazo::Signal::getOutput<Zuazo::Video>(testSrc);
+
+	//And the same signal delayed to the second
+	Zuazo::Signal::Delay<Zuazo::Video> delayLine("Delay", Zuazo::Duration(std::chrono::seconds(1)));
+	delayLine.getInput() << Zuazo::Signal::getOutput<Zuazo::Video>(testSrc);
+	Zuazo::Signal::getInput<Zuazo::Video>(window2) << delayLine.getOutput();
+
+	const auto updateCallback = std::make_shared<Zuazo::Instance::ScheduledCallback>(
+		[&instance, &delayLine] () {
+			delayLine.update(instance.getTime());
+		}
+	);
+	instance.addRegularCallback(updateCallback, Zuazo::Instance::PROCESSOR_PRIORITY);
+
+	//Done!
+	lock.unlock();
+	getchar();
+	lock.lock();
+
+	instance.removeRegularCallback(updateCallback);
+}
